242b9661244fa8334bb1c58da7bf758dedcad095
钱泽昊 <63004409+QianZeHao123@users.noreply.github.com>
2022-04-29 10:01:44 +0800

    Update drawPic.go

--- src/imagination/drawPic.go
+++ src/imagination/drawPic.go
@@ -1,5 +1,5 @@
 package main
- 
+
 import (
 	"fmt"
 	"image"
@@ -8,14 +8,14 @@ import (
 	"log"
 	"os"
 )
- 
+
 const (
 	dx = 500
 	dy = 200
 )
- 
+
 func main() {
- 
+
 	file, err := os.Create("test.jpeg")
 	if err != nil {
 		log.Fatal(err)
@@ -24,10 +24,10 @@ func main() {
 	alpha := image.NewAlpha(image.Rect(0, 0, dx, dy))
 	for x := 0; x < dx; x++ {
 		for y := 0; y < dy; y++ {
-			alpha.Set(x, y, color.Alpha{uint8(x % 256)})   //设定alpha图片的透明度
+			alpha.Set(x, y, color.Alpha{uint8(x % 256)}) //设定alpha图片的透明度
 		}
 	}
- 
+
 	fmt.Println(alpha.At(400, 100))    //144 在指定位置的像素
 	fmt.Println(alpha.Bounds())        //(0,0)-(500,200)，图片边界
 	fmt.Println(alpha.Opaque())        //false，是否图片完全透明
