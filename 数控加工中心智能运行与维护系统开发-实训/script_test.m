868ef5b834deb594b1810c44867ee9125f3b7fa8
钱泽昊 <63004409+QianZeHao123@users.noreply.github.com>
2022-10-18 19:27:00 +0800

    Data Process

--- 数控加工中心智能运行与维护系统开发-实训/script_test.m
+++
@@ -1,3 +0,0 @@
-for a = 10:2:20 
-   fprintf('value of a: %d\n', a);
-end
---
+++ 数控加工中心智能运行与维护系统开发-实训/statistic_data_process.m
@@ -0,0 +1,9 @@
+clear;clc;
+load('./DATA/data.mat');
+for n = 1:145
+    data(n).smcAC_max=max(data(n).smcAC);
+    data(n).smcAC_min=min(data(n).smcAC);
+    data(n).smcDC_max=max(data(n).smcDC);
+    data(n).smcDC_min=min(data(n).smcDC);
+end
+save data;
