07057f13f8a38b47a9b9e30b15035db7036a3b34
钱泽昊 <63004409+QianZeHao123@users.noreply.github.com>
2022-10-18 20:09:47 +0800

    fixed

--- 数控加工中心智能运行与维护系统开发-实训/statistic_data_process.m
+++ 数控加工中心智能运行与维护系统开发-实训/statistic_data_process.m
@@ -1,7 +1,9 @@
+
 clear;clc;
 load('./DATA/data.mat');
 
 for n = 1:145
+    % 
     data(n).smcAC_max = max(data(n).smcAC);
     data(n).smcDC_max = max(data(n).smcDC);
     data(n).smcAC_min = min(data(n).smcAC);
