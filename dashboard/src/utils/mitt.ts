19daccfca0b89be6af1d2bd9f83842df5bbaf5ed
qianzehao123 <qianzehao123@gmail.com>
2022-11-20 17:13:36 +0800

    delete dashboard

--- dashboard/src/utils/mitt.ts
+++
@@ -1,9 +0,0 @@
-// https://www.npmjs.com/package/mitt
-import mitt, { Emitter } from 'mitt';
-import { MittType } from '/@/types/mitt';
-
-// 类型
-const emitter: Emitter<MittType> = mitt<MittType>();
-
-// 导出
-export default emitter;
