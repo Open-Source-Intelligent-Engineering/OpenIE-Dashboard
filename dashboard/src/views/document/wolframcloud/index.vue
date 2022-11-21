02d7794c0aa08803fc2e0723f82446f5247d4fae
qianzehao123 <qianzehao123@gmail.com>
2022-11-22 01:07:44 +0800

    新增5个document中的页面

---
+++ dashboard/src/views/document/wolframcloud/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full">
+    <iframe class="wh-full" :src="src"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+const src = 'https://www.wolframcloud.com/';
+</script>
+<style scoped></style>
