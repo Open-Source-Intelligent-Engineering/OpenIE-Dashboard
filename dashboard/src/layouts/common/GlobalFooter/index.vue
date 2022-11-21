28e23166f74a6b1a3ce4e58d2270cfcbd58ee49e
qianzehao123 <qianzehao123@gmail.com>
2022-11-22 01:08:12 +0800

    更改全局角标

--- dashboard/src/layouts/common/GlobalFooter/index.vue
+++ dashboard/src/layouts/common/GlobalFooter/index.vue
@@ -1,6 +1,6 @@
 <template>
   <dark-mode-container class="flex-center h-full">
-    <span>Copyright ©2022 Computer and Industry Engineering Group</span>
+    <span>Copyright ©2022 开源工业工程基金会</span>
   </dark-mode-container>
 </template>
 
